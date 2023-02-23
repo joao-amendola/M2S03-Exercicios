ba2b326bbeae451d41ae387394bfb38cb60d49a5
João Amendola <mx.joaoo@gmail.com>
2023-02-23 14:46:59 -0300

    Exercício 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import philips.devinhouse.Clinica;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o peso (quilogramas):");
+        float peso = scanner.nextFloat();
+        System.out.println("Digite a altura (metros):");
+        float altura = scanner.nextFloat();
+
+        Clinica clinica = new Clinica();
+        clinica.CalcularIMC(peso, altura);
+
     }
 }
--- src/philips/devinhouse/Clinica.java
+++ src/philips/devinhouse/Clinica.java
@@ -5,4 +5,33 @@ public class Clinica {
     String cnpjClinica;
     String logradouroClinica;
     Integer codigoClinica;
+
+    public float CalcularIMC(float peso, float altura){
+
+        float imc = peso / (altura*altura);
+
+        System.out.println();
+        System.out.println("#####################");
+        System.out.println("      RESULTADO");
+        System.out.println("#####################");
+        System.out.println();
+        System.out.printf("IMC de %.2f => ", imc);
+
+        if (imc <= 18.5) {
+            System.out.println("MAGREZA");
+            return imc;
+        } else if (imc < 25) {
+            System.out.println("NORMAL");
+            return imc;
+        } else if (imc < 30) {
+            System.out.println("SOBREPESO");
+            return imc;
+        } else if (imc < 40) {
+            System.out.println("OBESIDADE");
+            return imc;
+        } else {
+            System.out.println("OBESIDADE GRAVE");
+            return imc;
+        }
+    }
 }
